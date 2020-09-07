800ff90301b8082eb28be4ee0f83e3aa8621bd70
Moses <lazaromoses77@gmail.com>
2020-09-07 10:35:41 +0300

    initial commit

---
+++ web/js/animate.js
@@ -0,0 +1,30 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1500,
+    distance: '35rem',
+    delay: 500
+})
+
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1500,
+    distance: '35rem',
+    delay: 500
+})
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1500,
+    distance: '35rem',
+    delay: 500
+})
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1500,
+    distance: '35rem',
+    delay: 500
+});
